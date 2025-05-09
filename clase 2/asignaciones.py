ace3607e2a8d4d377ae82fdba0c113d86cb3b054
cesarbarrios20 <cesar06arrios@gmail.com>
2025-05-09 00:03:22 -0300

    agregor operaciones.py

---
+++ clase 2/asignaciones.py
@@ -0,0 +1,14 @@
+'''
+Ejemplo de asignaciones en python
+'''
+c=0
+c=c+1 #primera forma de asignacion +1
+print(c) 
+
+a=0
+a+=1 #segunda forma de asignacion +1
+print(a)
+
+a=10
+a-=5 #seria para restar
+print(a)
---
+++ clase 2/salidadedatos.py
@@ -0,0 +1,6 @@
+'''
+Salida de datos
+'''
+app="python"
+proyecto="app.py"
+print(f"Se hara {app} se llmara {proyecto}")
